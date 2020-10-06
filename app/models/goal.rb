c5985b98fb719de84ba56bf0e7061d9995a5d8e3
AGehlot <andrew.gehlot@gmail.com>
2020-10-06 02:05:16 -0400

    Added commentable concern

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,8 @@
 class Goal < ApplicationRecord
+    include Commentable
+    
     validates :user_id, :goal_title, :description, presence: true
     belongs_to :user
-    has_many :comments, as: :commentable, dependent: :destroy
 
     def is_private?
         !self.public?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Commentable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   has_many :goals
-  has_many :comments, as: :commentable, dependent: :destroy
 end
